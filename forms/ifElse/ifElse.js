8093d07be7304968f3a92afb820ff10c65366838
KiaraMills <79479538+KiaraMills@users.noreply.github.com>
2021-02-28 22:31:10 -0600

    A2-branch, with the forms

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,30 @@
+//Defining the variables
+let firstName = ""
+let state = ""
+let temperature = ""
+
+// sets the pop up prompt 
+firstName = prompt("What is your first name?")
+state = prompt("Do you live in FL or NE?")
+temperature = prompt("What is the temperature?")
+
+//messages to be the output
+let message = ["Wear a warm coat, hat scarf and gloves." , "Wear a warm coat but you won't need a hat, scarf or gloves." , 
+"Wear your warmest coat, a warm hat, a scarf, and warm gloves." , "Wear  a warm coat, hat and gloves. Maybe a scarf too."]
+
+//statements in if/else situtations
+if(temperature < 32 && state == 'NE'){
+  console.log(`${firstName}, ${message[0]}.`)
+}
+else if(temperature > 32 && temperature < 50 && state == 'NE'){
+  console.log(`${firstName}, $(message[1]}.`)
+}
+else if(temperature > 32 && temperature <50 && state == 'FL'){
+  console.log(`${firstName}, ${message[2]}.`)
+}
+else if(temperature > 50 && temperature < 70 && state == 'FL'){
+  console.log(`${firstName}, ${messgae[3]}.`)
+}  
+else {
+  console.log('Invalid input.')
+}
